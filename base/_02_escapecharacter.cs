72e1881b69a760e9a4471d064da84d911a56214e
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2024-12-05 13:14:17 +0900

    Create _02_escapecharacter.cs
    이스케이프 문자

---
+++ base/_02_escapecharacter.cs
@@ -0,0 +1,16 @@
+using System;
+
+//프로그래머스 코테연습/코딩 기초 트레이닝/대소문자 바꿔서 출력하기
+//문자열에 특수문자 넣기 : 이스케이프 문자 
+// '," 앞에 백슬래쉬 \를 넣는다.
+namespace Programmers
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            String s = "!@#$%^&*(\\'\"<>?:;";
+            Console.Write(s);
+        }
+    }
+}
