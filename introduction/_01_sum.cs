e303c06c57b79df4665a786d7cf2c15215f89412
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2024-12-05 16:55:27 +0900

    Update _01_sum.cs
    프로그래머스 코딩테스트 입문 day1 문제

--- introduction/_01_sum.cs
+++ introduction/_01_sum.cs
@@ -3,6 +3,7 @@ using System;
 public class Solution {
     public int solution(int num1, int num2) {
         int answer = num1+num2;
+        //뒤의 입문 문제들 모두 2,3,4 다 사칙연산 이라서 - * /만 해주면 됨 
         return answer;
     }
 }
