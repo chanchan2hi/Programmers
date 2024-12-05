934466025576b821a1eac8a3d4bc11064f038170
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2024-12-05 17:07:38 +0900

    Create _03_NumEquals.cs
    수 같은지 비교

---
+++ introduction/_03_NumEquals.cs
@@ -0,0 +1,10 @@
+using System;
+
+//앞으로는 깃에 올릴때 대문자 이름 사용해 !
+public class Solution {
+    public int solution(int num1, int num2) {
+        if(num1==num2) return 1;
+        else return -1;
+      // return num1 == num2 ? 1 : -1; 이렇게 람다식 한 사람들이 많음.
+    }
+}
