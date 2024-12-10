f1c5d45a3bfa5ec10ab74360a6e2e70a6f3545ad
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2024-12-10 16:43:54 +0900

    Create _09_ArrToString.cs

---
+++ base/_09_ArrToString.cs
@@ -0,0 +1,16 @@
+using System;
+
+//프로그래머스 코테연습/코딩  트레이닝
+//문제: 기초 -문자 리스트를 문자열로 변환하기
+//
+namespace Programmers.Properties
+{
+    public class Solution
+    {  public string solution(string[] arr) {
+            string answer = "";
+            for(int i=0; i<arr.Length; i++)
+                answer+=arr[i].ToString();
+            return answer;
+        }
+    }
+}
