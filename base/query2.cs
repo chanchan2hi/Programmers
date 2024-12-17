edf660ba093fedb5f98c0e518aaa5af5be4f0d8d
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2024-12-17 17:36:37 +0900

    Create query2.cs

---
+++ base/query2.cs
@@ -0,0 +1,23 @@
+int[] answer = new int[queries.GetLength(0)];
+        int min;
+        bool isnone;
+        
+        for (int i = 0; i < queries.GetLength(0); i++) {
+            isnone = true;
+            min = -2;
+            
+            for (int j = queries[i, 0]; j <= queries[i, 1]; j++) {
+                if (arr[j] > queries[i, 2]) {
+                    if (arr[j] < min || min == -2) {
+                        if (isnone) {
+                            isnone = false;
+                        }
+                        min = arr[j];
+                    }
+                }
+            }
+            
+            answer[i] = (isnone) ? -1 : min;
+        }
+        
+        return answer;
