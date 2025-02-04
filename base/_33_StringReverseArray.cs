25979d78d319735bc73da539cff16955c3d164ce
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2025-02-04 18:42:58 +0900

    Update _33_StringReverseArray.cs

--- base/_33_StringReverseArray.cs
+++ base/_33_StringReverseArray.cs
@@ -14,3 +14,17 @@ public class Solution {
         return new string(arr);
     }
 }
+
+// using System;
+
+// public class Solution {
+//     public string solution(string my_string, int[,] queries) {
+//         char[] chr = my_string.ToCharArray();
+//         for(int i = 0; i<queries.GetLength(0); i++){
+//             int s = queries[i,0];
+//             int e = queries[i,1];
+//             Array.Reverse(chr, s, e-s+1);
+//         }
+//         return new string(chr);
+//     }
+// }
