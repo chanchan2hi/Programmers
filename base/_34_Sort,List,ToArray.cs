a228d77211d31b7388e9ecc7e3304361a6f553d7
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2025-08-01 20:32:32 +0900

    Create _34_Sort,List,ToArray.cs

---
+++ base/_34_Sort,List,ToArray.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public string[] solution(string my_string) {
+        List<string> answer = new List<string>();
+        for(int i=0; i<my_string.Length;i++){
+            answer.Add(my_string.Substring(i,my_string.Length-i));
+        }
+        answer.Sort();
+        return answer.ToArray();
+    }
+}
