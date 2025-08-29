7e518cecb97d0c46d4a88dfdb04575b9037c922f
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2025-08-29 21:15:24 +0900

    Create _65_대문자소문자로바꾸기.cs

---
+++ base/_65_대문자소문자로바꾸기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        return myString.ToUpper();
+    }
+}
+
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        return myString.ToLower();
+    }
+}
