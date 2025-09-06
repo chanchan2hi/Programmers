a988308ab0a702d21a36faca51053adcddbac7c4
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2025-09-06 21:10:37 +0900

    Create _74_rnystring.cs

---
+++ base/_74_rnystring.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string solution(string rny_string) {
+        if(rny_string.Contains("m")){
+            rny_string =rny_string.Replace("m","rn");
+        }
+       
+            return rny_string;
+           
+        
+       
+    }
+}
